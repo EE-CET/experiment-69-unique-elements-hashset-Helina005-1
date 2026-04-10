4c4e45f63184fb312c84aecfbf47b68328433cc3
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 22:10:08 +0530

    Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,38 @@
-import java.util.HashSet;
+
+   import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.Iterator;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
-        // TODO: Read the integer N
-        
-        // TODO: Create a HashSet to store the integers
-        
-        // TODO: Loop N times to read the integers and add them to the HashSet
-        
-        // TODO: Iterate through the HashSet and print the unique numbers (space-separated)
-        
-        scanner.close();
+        // HashSet only stores unique elements
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+
+        // 1. Read the number of integers N
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+
+            // 2. Read N integers and add them to the HashSet
+            for (int i = 0; i < n; i++) {
+                if (sc.hasNextInt()) {
+                    uniqueNumbers.add(sc.nextInt());
+                }
+            }
+
+            // 3. Print the unique numbers separated by space
+            Iterator<Integer> it = uniqueNumbers.iterator();
+            while (it.hasNext()) {
+                System.out.print(it.next());
+                if (it.hasNext()) {
+                    System.out.print(" ");
+                }
+            }
+            // Optional: Print a newline at the end
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
